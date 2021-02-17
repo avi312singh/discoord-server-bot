acb40e338409f396c9fb85ba049700ce7def4886
Aviraj Mandair <amandair@and.digital>
2021-02-17 20:03:04 +0000

    reverted last login and removed aync library

--- routesUtils/serverStatsUtils/lastLogin.js
+++ routesUtils/serverStatsUtils/lastLogin.js
@@ -1,5 +1,4 @@
 const moment = require('moment');
-const async = require('async')
 
 module.exports =
     (encodedNameToBeStored, pool) => {
@@ -10,26 +9,13 @@ module.exports =
                     pool.getConnection((err, connection) => {
                         const name = decodeURIComponent(encodedNameToBeStored);
                         if (err) console.log(err);
-
-                        async.parallel([
-                            (parallel_done) => {
-                                connection.query(`INSERT INTO playerInfo (playerName) VALUES (?) ON DUPLICATE KEY UPDATE totalTime = totalTime + .25, totalTimeDaily = totalTimeDaily + .25, lastLogin = '${timestampForLastLogin}'`,
-                                    [name], (err, results) => {
-                                        if (err) return parallel_done(err);
-                                        parallel_done();
-                                    })
-                            },
-                            (parallel_done) => {
-                                connection.query(`UPDATE playerInfo SET online = 0 WHERE playerName = ?`,
-                                    [name], (err, results) => {
-                                        if (err) return parallel_done(err);
-                                        parallel_done();
-                                    });
-                            }
-                        ], (err) => {
-                            if (err) console.log(err);
-                                connection.release();
-                                resolve({ name: name, lastLogin: timestampForLastLogin, online: false })
+                        connection.query(`INSERT INTO playerInfo (playerName, online) VALUES (?, false) ON DUPLICATE KEY UPDATE totalTime = totalTime + .25, totalTimeDaily = totalTimeDaily + .25, lastLogin = '${timestampForLastLogin}'`
+                        , [name], (err, result) => {
+                            connection.release();
+                            return err ? reject(err) : resolve({
+                                name: name, lastLogin: timestampForLastLogin, online: false
+                                // , result: result
+                            });
                         });
                     });
                 }
