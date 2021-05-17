3b50511b376b973de0f67b3a7c8845e16a33e7a7
Aviraj Mandair <amandair@and.digital>
2021-05-17 15:58:30 +0100

    added killCount endpoint for aggregated stats

--- routesUtils/aggregatedStatsUtils/killCount.js
+++ routesUtils/aggregatedStatsUtils/killCount.js
@@ -1,15 +1,17 @@
+const { result } = require("underscore");
+
 module.exports = (durationFromRequest, pool) => {
     return new Promise((resolve, reject) => {
         try {
             const durationCheckIfInParams = durationFromRequest ? durationFromRequest : 288;
-            const duration = durationCheckIfInParams !== '2016' && durationCheckIfInParams !== '8760' && durationCheckIfInParams !== '666'
+            const duration = durationCheckIfInParams !== '2016' && durationCheckIfInParams !== '8760' && durationCheckIfInParams !== '666' && durationCheckIfInParams !== '999'
                 ? 288 : parseInt(durationCheckIfInParams)
             pool.getConnection((err, connection) => {
                 if (err) console.log(err);
                 switch (duration) {
                     // weekly
                     case 2016:
-                        connection.query(`SELECT SUM(totalKillsWeekly) FROM sys.playerInfo;`,
+                        connection.query(`SELECT SUM(totalKillsWeekly) as totalKillsWeekly FROM sys.playerInfo;`,
                             (err, result) => {
                                 if (err) console.log(err);
                                 return err ? reject(err) : resolve({
@@ -22,7 +24,7 @@ module.exports = (durationFromRequest, pool) => {
                         break;
                     // monthly
                     case 8760:
-                        connection.query(`SELECT SUM(totalKillsMonthly) FROM sys.playerInfo;`,
+                        connection.query(`SELECT SUM(totalKillsMonthly) as totalKillsMonthly FROM sys.playerInfo;`,
                             (err, result) => {
                                 if (err) console.log(err);
                                 return err ? reject(err) : resolve({
@@ -35,7 +37,7 @@ module.exports = (durationFromRequest, pool) => {
                         break;
                     // all
                     case 666:
-                        connection.query(`SELECT SUM(totalKills) FROM sys.playerInfo;`,
+                        connection.query(`SELECT SUM(totalKills) as totalKills FROM sys.playerInfo;`,
                             (err, result) => {
                                 if (err) console.log(err);
                                 return err ? reject(err) : resolve({
@@ -48,13 +50,35 @@ module.exports = (durationFromRequest, pool) => {
                         break;
                     // daily
                     case 288:
-                        connection.query(`SELECT SUM(totalKillsDaily) FROM sys.playerInfo;`,
+                        connection.query(`SELECT SUM(totalKillsDaily) as totalKillsDaily FROM sys.playerInfo;`,
                             (err, result) => {
                                 if (err) console.log(err);
                                 return err ? reject(err) : resolve({
                                     duration,
                                     response: result
                                 });
+                            });
+                        connection.release();
+                        if (err) throw err;
+                        break;
+                    // all columns
+                    case 999:
+                        connection.query(`SELECT SUM(totalKillsDaily), SUM(totalKillsWeekly), SUM(totalKillsMonthly), SUM(totalKills) FROM sys.playerInfo;`,
+                            (err, result) => {
+                                if (err) console.log(err);
+                                const durations = [
+                                    'Today',
+                                    'Week',
+                                    'Month',
+                                    'All'
+                                ];
+                                return err ? reject(err) : resolve({
+                                    duration,
+                                    response: durations.map((duration, index) => {
+                                        return {
+                                        name: duration,
+                                        kills: Object.values(result[0])[index]}})
+                                })
                             });
                         connection.release();
                         if (err) throw err;
